74751e944b30475fa4273ee8e81606dd4143f57f
Cheickne Kanoute <kanoutecheickne31@gmail.com>
2025-05-08 16:52:26 +0000

    Create namespace.ts

---
+++ lib/routes/lib/routes/goal/namespace.ts
@@ -0,0 +1,7 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Goal.com',
+    url: 'https://www.goal.com/fr/news',
+    description: 'Actualités football sur Goal.com (France)',
+};
